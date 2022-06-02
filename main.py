bd4d2554eaa7434969e18ac1006ca371eaf7d0da
Mark <wehnsdaefflae@web.de>
2022-06-02 10:52:03 +0200

    clean up

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ class ImageBot:
         self.hashtag = ""
 
         self.started_at = round(time.time())
+        # self.started_at = -2
+
         self.processed = set()
         self.queue = dict()
 
@@ -81,19 +83,19 @@ class ImageBot:
             each_node = each_container["node"]
 
             if each_node.get("is_video", True) or each_node.get("taken_at_timestamp", -1) < self.started_at:             # false, 1653997832
-                log.info("skipping post: is video or was taken before system startup")
+                log.info("skipping node: is video or was taken before system startup")
                 continue
 
             # "https://instagram.ftxl3-1.fna.fbcdn.net/v/t51.2885-15/284866407_106157872074214_6314192356252402153_n.jpg?stp=dst-jpg_e35&_nc_ht=instagram.ftxl3-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=Og8G8GLwqZgAX882Aft&edm=ABZsPhsBAAAA&ccb=7-5&oh=00_AT_KlEEkb6oLhAr4eohHPH60DP7mBsXoesP2WPd7jXTyjg&oe=629C4D17&_nc_sid=4efc9f"
             image_url = each_node.get("display_url", None)
             if image_url is None:
-                log.info("skipping post: missing display_url")
+                log.info("skipping node: missing display_url")
                 continue
 
             # "2850262858630868528"
             post_id = each_node.get("id", None)
             if post_id is None or post_id in self.processed:
-                log.info("skipping post: missing post_id or already printed")
+                log.info("skipping node: missing post_id or already printed")
                 continue
 
             log.info(f"adding image '{post_id:s}' to print queue")
