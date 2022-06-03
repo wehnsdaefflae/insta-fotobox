743fb3cc5e4d5635469f383412008fd43b339ca8
Mark <wehnsdaefflae@web.de>
2022-06-03 14:41:18 +0200

    refactoring

--- bot_sandbox.py
+++ bot_sandbox.py
@@ -26,34 +26,40 @@ def main():
 
     browser.implicitly_wait(5)
 
+    # --- starting clicks
     necessary_cookies = browser.find_element(by=By.XPATH, value="/html/body/div[4]/div/div/button[1]")
     necessary_cookies.click()
 
     time.sleep(5)
 
+    # --- logging in
     user_name = browser.find_element(by=By.XPATH, value='//*[@id="loginForm"]/div/div[1]/div/label/input')
-    password = browser.find_element(by=By.XPATH, value='//*[@id="loginForm"]/div/div[2]/div/label/input')
-
     user_name.send_keys(config["instagram_username"])
+    password = browser.find_element(by=By.XPATH, value='//*[@id="loginForm"]/div/div[2]/div/label/input')
     password.send_keys(config["instagram_password"])
     password.submit()
 
     time.sleep(5)
 
-    browser.get(f"https://www.instagram.com/explore/tags/{config['hashtag']:s}/")
+    # --- getting image urls
+    for i in range(10):
+        browser.get(f"https://www.instagram.com/explore/tags/{config['hashtag']:s}/")
 
-    time.sleep(5)
+        time.sleep(5)
 
-    latest = browser.find_elements(by=By.XPATH, value='/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/article/div[2]/div//img')
+        latest = browser.find_elements(by=By.XPATH, value='/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/article/div[2]/div//img')
 
-    time.sleep(5)
+        time.sleep(5)
 
-    for each in latest:
-        print(each.get_property("src"))
+        image_urls = {each_child.get_property("src") for each_child in latest}
+        print(image_urls)
 
-    # By.CSS_SELECTOR
+        time.sleep(60)
 
     browser.close()
+
+    # selenium on headless raspberry pi
+    # https://stackoverflow.com/questions/25027385/using-selenium-on-raspberry-pi-headless
 
 
 if __name__ == "__main__":
